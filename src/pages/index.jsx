37e7ce843c79d4a38e22a67c4a290204f1990a69
zafar-m3n <thezafar.m3n@gmail.com>
2024-11-13 12:20:09 +0530

    Setup Craft.js

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,31 +10,47 @@ import { Button } from "../components/user/Button";
 import { Card } from "../components/user/Card";
 import { Text } from "../components/user/Text";
 
+import { Editor, Frame, Element } from "@craftjs/core";
+
 export default function App() {
   return (
     <div className="mx-auto">
       <h1 className="text-xl font-semibold text-center mt-4">
         Cleveri Web Page Builder
       </h1>
-      <div className="grid grid-cols-4 gap-4 pt-4">
-        {/* Topbar */}
-        <div className="col-span-4">
-          <Topbar />
+      <Editor resolver={{ Card, Button, Text, Container }}>
+        <div className="grid grid-cols-4 gap-4 pt-4">
+          <div className="col-span-4">
+            <Topbar />
+          </div>
+          <div className="col-span-3">
+            <Frame>
+              <Element
+                is={Container}
+                padding={5}
+                background="bg-gray-200"
+                canvas
+              >
+                <Card />
+                <Button size="small">Click</Button>
+                <Text fontSize="small" text="Hi world!" />
+                <Element
+                  is={Container}
+                  padding={6}
+                  background="bg-gray-400"
+                  canvas
+                >
+                  <Text fontSize="small" text="It's me again!" />
+                </Element>
+              </Element>
+            </Frame>
+          </div>
+          <div className="col-span-1 bg-white p-2 rounded shadow">
+            <Toolbox />
+            <SettingsPanel />
+          </div>
         </div>
-
-        {/* Main content area */}
-        <div className="col-span-3">
-          <Container padding={5} background="bg-gray-200">
-            <Card />
-          </Container>
-        </div>
-
-        {/* Sidebar */}
-        <div className="col-span-1 bg-white p-2 rounded shadow">
-          <Toolbox />
-          <SettingsPanel />
-        </div>
-      </div>
+      </Editor>
     </div>
   );
 }
